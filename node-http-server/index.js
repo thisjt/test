5ad70e97cd6e4dc53ec3a560c8604731ac417f03
Joshua Tahanlangit <contact@thisjt.me>
2024-06-23 12:01:33 +0800

    feat: node http server

---
+++ node-http-server/index.js
@@ -0,0 +1,9 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hello World\n');
+});
+
+server.listen(3000);
+console.log('Running on localhost:3000');
