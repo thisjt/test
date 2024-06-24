b231267e170e7cf9921ddb6f7ba20787f39e654a
Joshua Tahanlangit <contact@thisjt.me>
2024-06-24 14:34:51 +0800

    feat: node http test

--- node-http-server/index.js
+++ node-http-server/index.js
@@ -1,9 +1,16 @@
 import http from 'http';
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello World\n');
+const server = http.createServer((request, response) => {
+	if (request.url === '/favicon.ico') {
+		response.writeHead(200, { 'content-type': 'image/x-icon' });
+		response.end(Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64'));
+		return;
+	}
+
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+	console.log(`Accessed: ${new Date().toLocaleString()} - ${request.url}`);
+	response.end('ok\n');
 });
 
-server.listen(3000);
-console.log('Running on localhost:3000');
+server.listen(80);
+console.log('Running on localhost:80');
